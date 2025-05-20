f9c4f62f5878ff4d84253fe02d6c7afcac6d0913
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-05-20 10:56:35 +0900

    feat: 스프링 프로젝트 초기 설정

---
+++ src/main/java/kr/tatine/manibogo_oms_v2/ManibogoOmsV2Application.java
@@ -0,0 +1,13 @@
+package kr.tatine.manibogo_oms_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManibogoOmsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManibogoOmsV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/tatine/manibogo_oms_v2/ManibogoOmsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.tatine.manibogo_oms_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManibogoOmsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
