1fb430a3409d50cbc71bc7bbcfae5b0dc341e755
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-03 17:22:28 +0900

    feat: 상품주문 연동 서비스 코드 구현
    - 상품주문을 중심으로, 연관 도메인이 없는 경우 생성하는 로직 구현
    AR 간 트랜잭션을 나눈 것은 수정시 트랜잭션 락 및 경합을 방지하기 위함이다. 생성 시에는 경합이 발생하지 않으므로 서비스 안에서 여러 AR를 생성할 수 있다.
    (https://stackoverflow.com/questions/32082479/ddd-create-one-aggregate-root-within-another-ar)
    (https://stackoverflow.com/questions/33283095/how-to-handle-getorcreateifabsent-inside-a-ddd-aggregate)
    
    - 주문, 상품주문, 옵션, 생성 등 각 도메인 객체에 대한 레파지토리 객체 추가
    - 상품의 초기 우선순위(Priority) 조회 및 생성 로직 추가
    - 옵션의 ID를 옵션키: 이름으로 변경

---
+++ src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/command/application/InvalidOptionFormatException.java
@@ -0,0 +1,3 @@
+package kr.tatine.manibogo_oms_v2.fulfillment.command.application;
+
+public class InvalidOptionFormatException extends RuntimeException { }
--- src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/command/domain/item_order/ItemOrder.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/command/domain/item_order/ItemOrder.java
@@ -1,12 +1,17 @@
 package kr.tatine.manibogo_oms_v2.fulfillment.command.domain.item_order;
 
 import jakarta.persistence.*;
+import kr.tatine.manibogo_oms_v2.common.model.Money;
+import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.option.OptionId;
+import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.order.OrderNumber;
+import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.product.ProductNumber;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @ToString
@@ -16,11 +21,21 @@ public class ItemOrder {
     @EmbeddedId
     private ItemOrderNumber number;
 
+    @Embedded
+    private OrderNumber orderNumber;
+
     @Enumerated(EnumType.STRING)
     private ItemOrderState state;
 
-    @Embedded
-    private Item item;
+    private ProductNumber productNumber;
+
+    @ElementCollection
+    @CollectionTable(name = "item_order_option", joinColumns = @JoinColumn(name = "item_order_number"))
+    private List<OptionId> optionIds;
+
+    private Integer amount;
+
+    private Money totalPrice;
 
     @Embedded
     private ShippingInfo shippingInfo;
@@ -32,10 +47,17 @@ public class ItemOrder {
 
     private LocalDate dispatchDeadline;
 
-    public ItemOrder(ItemOrderNumber number, Item item, ShippingInfo shippingInfo, LocalDate dispatchDeadline) {
+    public ItemOrder(ItemOrderNumber number, OrderNumber orderNumber, ProductNumber productNumber, List<OptionId> optionIds, Integer amount, Money totalPrice, ShippingInfo shippingInfo, LocalDate dispatchDeadline) {
         this.number = number;
+        this.orderNumber = orderNumber;
+
         setState(ItemOrderState.PLACED);
-        this.item = item;
+
+        this.productNumber = productNumber;
+        this.optionIds = optionIds;
+        this.amount = amount;
+        this.totalPrice = totalPrice;
+
         setShippingInfo(shippingInfo);
         setPreferredShipsOn(preferredShipsOn);
         setDispatchDeadline(dispatchDeadline);
