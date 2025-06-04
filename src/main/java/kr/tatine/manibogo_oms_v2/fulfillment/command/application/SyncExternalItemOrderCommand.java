c466c4a4b67a08b8008d2831351bb0bce0c03b64
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-04 12:35:53 +0900

    refactor: 외부 상품주문 커맨드 및 요청 객체 분리
    - 표현 영역이 애플리케이션 영역에 의존하지 않도록 커맨드 객체와 동일하게 요청 객체를 생성하였음

--- src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/command/application/SyncExternalItemOrderCommand.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/command/application/SyncExternalItemOrderCommand.java
@@ -1,13 +1,6 @@
 package kr.tatine.manibogo_oms_v2.fulfillment.command.application;
 
 import kr.tatine.manibogo_oms_v2.common.model.Money;
-import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.item_order.ItemOrderNumber;
-import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.item_order.ShippingInfo;
-import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.order.Customer;
-import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.order.OrderNumber;
-import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.order.Recipient;
-import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.order.SalesChannel;
-import kr.tatine.manibogo_oms_v2.fulfillment.command.domain.product.ProductNumber;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
