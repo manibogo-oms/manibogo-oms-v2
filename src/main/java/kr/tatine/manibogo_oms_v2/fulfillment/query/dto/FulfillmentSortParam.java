f369ca4529ff0b74c0cfb3dd1a0dae9d2fcb6121
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-10 14:03:02 +0900

    feat: 정렬 가능한 필드 Enum화, 다음 정렬 값 계산
    - Controller 영역에서 현재 정렬 값을 기반으로, 다음 정렬할 값을 미리 계산 (내림차순 > 오름차순 > 없음)
    - 이를 위해 정렬 가능한 필드를 Enum으로 정의함
    - 뷰 레이어에서 다음 정렬 값을 기반으로 현재 정렬 여부(UP, DOWN) 표시
    - 현재 URI에서 쿼리만 변경하는 `replaceOrAddParam` 빈 정의

---
+++ src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/query/dto/FulfillmentSortParam.java
@@ -0,0 +1,16 @@
+package kr.tatine.manibogo_oms_v2.fulfillment.query.dto;
+
+public enum FulfillmentSortParam {
+
+    SHIPPING_REGION_NAME,
+    PLACED_ON,
+    DISPATCH_DEADLINE,
+    PREFERRED_SHIPS_ON,
+    PURCHASED_ON,
+    DISPATCHED_ON,
+    SHIPPED_ON,
+    CONFIRMED_ON,
+    CANCELLED_ON,
+    REFUNDED_ON;
+
+}
