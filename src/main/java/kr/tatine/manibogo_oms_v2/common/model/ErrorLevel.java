131c57354e88b7cfd2cb9fc6a2ec68fe89b4af3d
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-17 13:25:27 +0900

    feat: 에러 결과에 레벨 정보 추가
    - 에러의 경증을 Level(WARN, ERROR) 로 표현하는 필드를 추가
    - Level에 따라 필드 및 전역 에러 스타일 적용

---
+++ src/main/java/kr/tatine/manibogo_oms_v2/common/model/ErrorLevel.java
@@ -0,0 +1,7 @@
+package kr.tatine.manibogo_oms_v2.common.model;
+
+public enum ErrorLevel implements Comparable<ErrorLevel> {
+
+    NONE, WARN, ERROR;
+
+}
--- src/main/java/kr/tatine/manibogo_oms_v2/common/model/ErrorResult.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/common/model/ErrorResult.java
@@ -4,43 +4,74 @@ import lombok.Getter;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Getter
 public class ErrorResult {
 
-    private final MultiValueMap<String, Message> fieldErrors = new LinkedMultiValueMap<>();
+    private final MultiValueMap<String, ErrorObject> fieldErrors = new LinkedMultiValueMap<>();
 
-    private final List<Message> globalErrors = new ArrayList<>();
+    private final List<ErrorObject> globalErrors = new ArrayList<>();
 
-    public void rejectValue(String fieldName, String errorCode, Object[] arguments) {
-        fieldErrors.add(fieldName, new Message(errorCode, arguments));
+    public void rejectValue(String fieldName, ErrorLevel errorLevel, String errorCode, Object[] arguments) {
+        fieldErrors.add(fieldName, new ErrorObject(errorLevel, new Message(errorCode, arguments)));
+    }
+
+    public void rejectValue(String fieldName, ErrorLevel errorLevel, String errorCode) {
+        rejectValue(fieldName, errorLevel, errorCode, new Object[0]);
+    }
+
+    public void rejectValue(String fieldName, String errorCode,  Object[] arguments) {
+        rejectValue(fieldName, ErrorLevel.ERROR, errorCode, arguments);
     }
 
     public void rejectValue(String fieldName, String errorCode) {
-        rejectValue(fieldName, errorCode, new Object[0]);
+        rejectValue(fieldName, ErrorLevel.ERROR, errorCode, new Object[0]);
+    }
+
+    public void reject(ErrorLevel errorLevel, String errorCode, Object[] arguments) {
+        globalErrors.add(new ErrorObject(errorLevel, new Message(errorCode, arguments)));
     }
 
     public void reject(String errorCode, Object[] arguments) {
-        globalErrors.add(new Message(errorCode, arguments));
+        reject(ErrorLevel.ERROR, errorCode, new Object[0]);
+    }
+
+    public void reject(ErrorLevel errorLevel, String errorCode) {
+        reject(errorLevel, errorCode, new Object[0]);
     }
 
     public void reject(String errorCode) {
-        reject(errorCode, new Object[0]);
+        reject(ErrorLevel.ERROR, errorCode, new Object[0]);
     }
 
     public boolean hasGlobalError() {
         return !globalErrors.isEmpty();
     }
 
-    public List<Message> getFieldErrors(String fieldName) {
+    public List<ErrorObject> getFieldErrors(String fieldName) {
         return fieldErrors.getOrDefault(fieldName, List.of());
     }
 
-    public boolean hasObjectError(String objectName) {
-        return fieldErrors.keySet().stream()
-                .anyMatch(key -> key.startsWith(objectName));
+    public ErrorLevel getHighestErrorLevel(String fieldName) {
+        if (!fieldErrors.containsKey(fieldName)) return ErrorLevel.NONE;
+
+        return fieldErrors.get(fieldName).stream()
+                .max(Comparator.comparing(ErrorObject::level))
+                .map(ErrorObject::level)
+                .orElse(ErrorLevel.NONE);
+
+    }
+
+
+    public ErrorLevel getHighestErrorLevelInObject(String objectName) {
+        return fieldErrors.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(objectName))
+                .map(Map.Entry::getValue)
+                .flatMap(List::stream)
+                .max(Comparator.comparing(ErrorObject::level))
+                .map(ErrorObject::level)
+                .orElse(ErrorLevel.NONE);
     }
 
 }
