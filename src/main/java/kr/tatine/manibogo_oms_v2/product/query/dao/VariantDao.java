603c70aea8eb23760c1e0ad41d111fdf3f90d0ba
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-20 15:24:52 +0900

    feat: 상품주문 옵션 동적 렌더링
    - 상품주문, 주문을 위한 Form 객체 생성
    - 더미 데이터로 상품주문 옵션 동적 렌더링 & 전송 확인

--- src/main/java/kr/tatine/manibogo_oms_v2/product/query/dao/VariantDao.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/product/query/dao/VariantDao.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
+
 public interface VariantDao extends Repository<Variant, VariantId> {
 
     @Query("SELECT new kr.tatine.manibogo_oms_v2.product.query.dto.VariantDto(" +
@@ -18,5 +20,13 @@ public interface VariantDao extends Repository<Variant, VariantId> {
             "FROM Variant v " +
             "WHERE v.variantId.productNumber.productNumber = :productNumber")
     Page<VariantDto> findAllByProductNumber(Pageable pageable, String productNumber);
+
+    @Query("SELECT new kr.tatine.manibogo_oms_v2.product.query.dto.VariantDto(" +
+            "   v.variantId.productNumber.productNumber, " +
+            "   v.variantId.option.key, " +
+            "   v.variantId.option.value, " +
+            "   v.label) " +
+            "FROM Variant v ")
+    List<VariantDto> findAll();
 
 }
