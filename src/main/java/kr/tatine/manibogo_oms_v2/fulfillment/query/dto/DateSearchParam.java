d8ed924812fad935ba8d7f41b2829a5f934353f0
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-21 17:53:14 +0900

    refactor: 주문상태 데이터 정렬시 시간 정보를 포함

--- src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/query/dto/DateSearchParam.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/fulfillment/query/dto/DateSearchParam.java
@@ -11,12 +11,12 @@ public enum DateSearchParam implements Describable {
     PLACED_AT("주문일자"),
     DISPATCH_DEADLINE("발송기한"),
     PREFERRED_SHIPS_ON("희망배송일자"),
-    PURCHASED_ON("발주일자"),
-    DISPATCHED_ON("출고일자"),
-    SHIPPED_ON("배송일자"),
-    CONFIRMED_ON("완료일자"),
-    CANCELLED_ON("취소일자"),
-    REFUNDED_ON("반품일자");
+    PURCHASED_AT("발주일자"),
+    DISPATCHED_AT("출고일자"),
+    SHIPPED_AT("배송일자"),
+    CONFIRMED_AT("완료일자"),
+    CANCELLED_AT("취소일자"),
+    REFUNDED_AT("반품일자");
 
     private final String description;
 
