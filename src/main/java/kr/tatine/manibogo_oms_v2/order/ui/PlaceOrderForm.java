65cea14011a47f276be5944c97eaa720ad50fdff
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-21 15:44:58 +0900

    refactor: 주문추가 폼 단일 상품만 처리하도록 변경
    - 상품 추가는 생성 후 "발주상품 추가" 폼을 렌더링해 구현하도록 함

--- src/main/java/kr/tatine/manibogo_oms_v2/order/ui/PlaceOrderForm.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/order/ui/PlaceOrderForm.java
@@ -2,21 +2,17 @@ package kr.tatine.manibogo_oms_v2.order.ui;
 
 import kr.tatine.manibogo_oms_v2.order.command.domain.model.vo.ChargeType;
 import kr.tatine.manibogo_oms_v2.order.command.domain.model.vo.ShippingMethod;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-public class AddOrderForm {
+public class PlaceOrderForm {
 
     private String customerName;
 
@@ -36,6 +32,14 @@ public class AddOrderForm {
 
     private String recipientAddress2;
 
+    private String productNumber;
+
+    private Integer amount;
+
+    private List<PlaceOrderOptionForm> options;
+
+    private String purchaseMemo;
+
     private ShippingMethod shippingMethod;
 
     private ChargeType shippingChargeType;
@@ -48,6 +52,16 @@ public class AddOrderForm {
 
     private String shippingMemo;
 
-    private List<AddItemOrderForm> itemOrderForms = new ArrayList<>();
+    @Getter
+    @Setter
+    @ToString
+    @NoArgsConstructor
+    public static class PlaceOrderOptionForm {
+
+        private String key;
+
+        private String value;
+
+    }
 
 }
