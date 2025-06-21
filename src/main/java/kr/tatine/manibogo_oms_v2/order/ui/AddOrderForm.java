d1870e5430e1d4cb028c99a7b8da557d38a36377
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-21 10:26:06 +0900

    feat: 주문 추가 폼 객체 UI와 바인딩

--- src/main/java/kr/tatine/manibogo_oms_v2/order/ui/AddOrderForm.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/order/ui/AddOrderForm.java
@@ -1,10 +1,14 @@
 package kr.tatine.manibogo_oms_v2.order.ui;
 
+import kr.tatine.manibogo_oms_v2.order.command.domain.model.vo.ChargeType;
+import kr.tatine.manibogo_oms_v2.order.command.domain.model.vo.ShippingMethod;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +17,36 @@ import java.util.List;
 @ToString
 @NoArgsConstructor
 public class AddOrderForm {
+
+    private String customerName;
+
+    private String customerTel;
+
+    private String customerMemo;
+
+    private String recipientName;
+
+    private String recipientTel1;
+
+    private String recipientTel2;
+
+    private String recipientZipCode;
+
+    private String recipientAddress1;
+
+    private String recipientAddress2;
+
+    private ShippingMethod shippingMethod;
+
+    private ChargeType shippingChargeType;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dispatchDeadline;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate preferredShipsOn;
+
+    private String shippingMemo;
 
     private List<AddItemOrderForm> itemOrderForms = new ArrayList<>();
 
