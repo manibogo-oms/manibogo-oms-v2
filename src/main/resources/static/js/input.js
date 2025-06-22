a25497a6cd9d5e73b7275510a90708bf21f36330
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-06-22 15:04:06 +0900

    feat: 다음 주소 팝업 API 연동
    - 키 발급이 필요없고, 연동이 쉬운 다음 주소 API를 사용하였음

--- src/main/resources/static/js/input.js
+++ src/main/resources/static/js/input.js
@@ -1,21 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const textarea = document.querySelector('.form-control.auto-height')
 
-    function adjustTextareaHeight(el) {
-        el.style.height = 'auto'; // 높이 초기화
-        el.style.height = el.scrollHeight + 'px'; // 스크롤 높이로 설정
-    }
+document.getElementById('btnSearchAddress')
+    .addEventListener('click', () => {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            console.log(data);
+            document.getElementById('recipientAddress1').value = data.address;
+            document.getElementById('recipientZipCode').value = data.zonecode;
+        }
+    }).open();
 
-    // 초기 로드 시 높이 조절 (이미 내용이 있는 경우)
-    adjustTextareaHeight(textarea);
-
-    // 입력 시 높이 조절
-    textarea.addEventListener('input', function() {
-        adjustTextareaHeight(this);
-    });
-
-    // (선택 사항) 창 크기 조절 시에도 높이 조절 (텍스트 줄바꿈 변경 시)
-    window.addEventListener('resize', function() {
-        adjustTextareaHeight(textarea);
-    });
 });
