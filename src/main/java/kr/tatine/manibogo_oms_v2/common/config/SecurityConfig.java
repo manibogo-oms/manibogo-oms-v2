36877b4445eff7e899d7af13777d82180f694ce4
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-07-18 11:31:10 +0900

    feat: 자동 로그인 토큰 영속화를 위해 Redis로 저장소 구현

--- src/main/java/kr/tatine/manibogo_oms_v2/common/config/SecurityConfig.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/common/config/SecurityConfig.java
@@ -2,22 +2,26 @@ package kr.tatine.manibogo_oms_v2.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, RedisTemplate<String, String> redisTemplate) throws Exception {
         http.authorizeHttpRequests(request -> request
                 .requestMatchers("/v2/**").authenticated()
                 .requestMatchers("/error", "/image/**", "/js/**", "/css/**").permitAll());
 
         http.formLogin(Customizer.withDefaults());
+
+        http.rememberMe(rmc -> rmc.tokenRepository(persistentTokenRepository(redisTemplate)));
 
         return http.build();
     }
@@ -25,6 +29,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(RedisTemplate<String, String> redisTemplate) {
+        return new RedisPersistentTokenRepository(redisTemplate);
     }
 
 }
