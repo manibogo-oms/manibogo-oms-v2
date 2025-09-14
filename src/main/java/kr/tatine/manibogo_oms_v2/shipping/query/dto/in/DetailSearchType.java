d18d17db6679797e8c0ed5559f4e48a5c11efb77
Heebeom Yang <takeny1998@gmail.com>
2025-09-14 18:53:30 +0900

    feat: 배송 검색, 필터링 구현

---
+++ src/main/java/kr/tatine/manibogo_oms_v2/shipping/query/dto/in/DetailSearchType.java
@@ -0,0 +1,12 @@
+package kr.tatine.manibogo_oms_v2.shipping.query.dto.in;
+
+public enum DetailSearchType {
+
+    SHIPPING_NUMBER,
+    RECIPIENT_NAME,
+    RECIPIENT_TEL1,
+    RECIPIENT_TEL2,
+    ADDRESS1,
+    ADDRESS2,
+
+}
---
+++ src/main/java/kr/tatine/manibogo_oms_v2/shipping/query/dto/in/ShippingFilter.java
@@ -0,0 +1,11 @@
+package kr.tatine.manibogo_oms_v2.shipping.query.dto.in;
+
+import kr.tatine.manibogo_oms_v2.shipping.command.domain.ShippingState;
+
+public record ShippingFilter(
+        DetailSearchType detailSearchType,
+        String keyword,
+        ShippingState state,
+        String sido,
+        String sigungu
+) { }
