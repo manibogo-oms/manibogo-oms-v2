71b968698990c257aa321dc45add18a801193773
Heebeom Yang <takeny1998@gmail.com>
2025-09-16 15:50:07 +0900

    feat: 페이니제이션 UI 컨트롤 구현
    - 페이지 정보, 크기 변경 등 헤더 구현
    - 페이니제이션 컨트롤 구현

--- src/main/java/kr/tatine/manibogo_oms_v2/shipping/ui/ShippingController.java
+++ src/main/java/kr/tatine/manibogo_oms_v2/shipping/ui/ShippingController.java
@@ -1,5 +1,6 @@
 package kr.tatine.manibogo_oms_v2.shipping.ui;
 
+import kr.tatine.manibogo_oms_v2.common.ui.PageView;
 import kr.tatine.manibogo_oms_v2.shipping.query.dto.in.ShippingFilter;
 import kr.tatine.manibogo_oms_v2.shipping.query.dto.out.ShippingPageView;
 import kr.tatine.manibogo_oms_v2.shipping.query.ShippingQueryService;
@@ -9,9 +10,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -21,15 +23,14 @@ public class ShippingController {
 
     private final ShippingQueryService queryService;
 
-
     @GetMapping
-    @ResponseBody
-    public void page(ShippingFilter filter, @PageableDefault Pageable pageable) {
-        Page<ShippingPageView> all = queryService.findAll(filter, pageable);
+    public String page(Model model,
+            @ModelAttribute(name = "filter") ShippingFilter filter,
+            @PageableDefault Pageable pageable) {
+        final Page<ShippingPageView> page = queryService.findAll(filter, pageable);
 
-        for (ShippingPageView shippingPageView : all.getContent()) {
-            log.info(shippingPageView.toString());
-        }
+        model.addAttribute("page", PageView.of(page));
+        return "shippings";
     }
 
 }
--- src/main/resources/static/js/pagenation.js
+++ src/main/resources/static/js/pagenation.js
@@ -2,9 +2,7 @@
 document.getElementById('selectPageSize')
 .addEventListener('change', (e) => {
     const link = document.getElementById('btnChangePageSize');
-
     const href = e.target.value;
-    console.log(href);
 
     link.setAttribute('href', href);
 })
